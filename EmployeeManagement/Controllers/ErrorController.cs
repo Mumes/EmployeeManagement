0231ba2eb840f8fc4e5a47497f6d53e91d733335
RodionovMI <RodionovMI@RODIONOVMI>
2019-11-03 13:04:58 +0300

    committest

--- EmployeeManagement/Controllers/ErrorController.cs
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +13,12 @@ namespace EmployeeManagement.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger logger;
+
+        public ErrorController(ILogger logger)
+        {
+            this.logger = logger;
+        }
         // GET: /<controller>/
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int? statusCode = null)
